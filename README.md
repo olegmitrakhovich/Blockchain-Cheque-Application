9f05ff05e1f70b5b4bd7827c0995fbd133b6db33
Oleg Mitrakhovich <olegmit9@hotmail.com>
2019-03-19 23:40:35 -0400

    beta commit
    work in progress....

--- README.md
+++
@@ -1,1 +0,0 @@
-"# Blockchain-Cheque-Application" 
--- app.js
+++ app.js
@@ -1,13 +1,42 @@
-<script>
-const Web3 = require('web3');
-const web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/8122b2a699f049d69d5f483ec7696d3c"));
+ //npm install --global --production windows-build-tools
+ //npm --add-python-to-path='true' --debug install --global windows-build-tools
+ //npm install web3@^0.20.0
+ //npm install -g ethereumjs-abi
+ //https://ethereum.stackexchange.com/questions/54883/updated-how-to-see-output-of-web3-personal-sign-in-browser-method-p
+ //npm install lite-server --save-dev
 
-function signPayment(recipient, amount, nonce, contractAddress, callback) {
-  var hash = "0x" + ethereumjs.ABI.soliditySHA3(
-    ["address", "uint256", "uint256", "address"],
-    [recipient, amount, nonce, contractAddress]
-  ).toString("hex");
 
-  web3.personal.sign(hash, web3.eth.defaultAccount, callback);
-}
-</script>
+
+const web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/infura-key"));
+
+
+   	function formValidation() {
+
+	var errMessages = "";
+
+    var recipient = document.forms["form8"]["RECIPIENT"].value;
+    var amount = document.forms["form8"]["AMOUNT"].value; 
+    var nonce = document.forms["form8"]["NONCE"].value;
+    var contractAddress = document.forms["form8"]["CONTRACTADDRESS"].value; 
+
+	signPayment(recipient, amount, nonce, contractAddress);
+
+	}
+                                                            
+	async function signPayment(recipient, amount, nonce, contractAddress) {
+         
+                              
+         var hash = web3.utils.soliditySha3(recipient, amount, nonce, contractAddress);
+         
+         alert("THIS IS YOUR MESSAGE HASH: " + hash);
+
+         web3.eth.accounts.sign(hash, 'PRIVATE KEY');
+         
+         
+
+    }
+
+   
+
+
+	
